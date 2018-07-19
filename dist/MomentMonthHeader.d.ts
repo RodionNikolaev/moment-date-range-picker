6716e36bbedf869bcab0e2c450d5934726ab7991
Rodion Nikolav <rodion@thinksoft.ru>
2018-07-19 21:32:42 +0300

    Remove files

--- dist/MomentMonthHeader.d.ts
+++
@@ -1,9 +0,0 @@
-import { Moment } from "moment";
-import * as React from "react";
-export interface MonthHeaderProps {
-    month: Moment;
-}
-export declare class MonthHeader extends React.Component<MonthHeaderProps, any> {
-    constructor(props: MonthHeaderProps);
-    render(): JSX.Element;
-}
--- example/app/app.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from "react-dom";
-import Index from './components/Index';
-
-window.onload = function () {
-    ReactDOM.render(<Index/>, document.getElementById("container"));
-};
